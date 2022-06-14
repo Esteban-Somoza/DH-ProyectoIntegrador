9a1a03edaad6df44dc5bd244005887a45149b346
“Esteban-Somoza” <estfersomoza@gmail.com>
2022-06-14 20:28:49 -0300

    primer aproximacion productfinder

--- app.js
+++ app.js
@@ -7,5 +7,6 @@ let public = path.resolve(__dirname, './public')
 server.use(express.static(public))
 
 server.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/home.html')));
+server.get('/productos', (req, res) => res.sendFile(path.join(__dirname, './views/productFinder.html')));
 // server.get('/curso', (req, res) => res.send("Bienvenido al curso de nodejs"))
 
