4b752eb3453fde81cd18987c6b35709abca5413a
“Esteban-Somoza” <estfersomoza@gmail.com>
2022-06-15 13:13:10 -0300

    productCart primera aproximacion

--- app.js
+++ app.js
@@ -9,5 +9,6 @@ server.use(express.static(public))
 server.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
 server.get('/productos', (req, res) => res.sendFile(path.join(__dirname, './views/productDetail.html')));
 server.get('/buscador', (req, res) => res.sendFile(path.join(__dirname, './views/productSearch.html')));
+server.get('/carrito', (req, res) => res.sendFile(path.join(__dirname, './views/productCart.html')));
 // server.get('/curso', (req, res) => res.send("Bienvenido al curso de nodejs"))
 
