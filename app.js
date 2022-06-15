03c2526b2c022b735914e2211d8c84e44573b426
“Esteban-Somoza” <estfersomoza@gmail.com>
2022-06-15 10:36:12 -0300

    pagina productDetail
    eliminado carpeta materiales

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let public = path.resolve(__dirname, './public')
 server.use(express.static(public))
 
 server.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
-
-server.get('/productos', (req, res) => res.sendFile(path.join(__dirname, './views/productFinder.html')));
+server.get('/productos', (req, res) => res.sendFile(path.join(__dirname, './views/productDetail.html')));
+server.get('/buscador', (req, res) => res.sendFile(path.join(__dirname, './views/productSearch.html')));
 // server.get('/curso', (req, res) => res.send("Bienvenido al curso de nodejs"))
 
