6fc432dd2a9ede67de093da1c66c9d6606418221
“Esteban-Somoza” <estfersomoza@gmail.com>
2022-06-16 21:05:13 -0300

    directorios rutas

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const express = require('express');
-const server = express();
+const app = express();
+const {port, callback} = require("./modules/listen.js");
 
-server.listen(3000,() => console.log('Abiendo servidor en http://localhost:3000'))
-let public = path.resolve(__dirname, './public')
-server.use(express.static(public))
+// const public = path.resolve(__dirname, './public')
+// app.use(express.static(public))
+const public = require('./modules/public.js');
+app.use(public)
 
-server.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
-server.get('/productos', (req, res) => res.sendFile(path.join(__dirname, './views/productDetail.html')));
-server.get('/buscador', (req, res) => res.sendFile(path.join(__dirname, './views/productSearch.html')));
-server.get('/carrito', (req, res) => res.sendFile(path.join(__dirname, './views/productCart.html')));
-// server.get('/curso', (req, res) => res.send("Bienvenido al curso de nodejs"))
+// app.listen(3000,() => console.log('Abiendo servidor en http://localhost:3000'))
+app.listen(port, callback(`Abiendo servidor en http://localhost:` + port))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
+app.get('/productos', (req, res) => res.sendFile(path.join(__dirname, './views/productDetail.html')));
+app.get('/buscador', (req, res) => res.sendFile(path.join(__dirname, './views/productSearch.html')));
+app.get('/carrito', (req, res) => res.sendFile(path.join(__dirname, './views/productCart.html')));
 
---
+++ modules/public.js
@@ -0,0 +1,7 @@
+const {static} = require('express');
+const {join} = require('path');
+
+let public = static(join(__dirname, '../public'));
+
+module.exports = public 
+
