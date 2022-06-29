df8977e68f5b9c1a7c6218e8eec54e8f125aa023
LuchoGiuliani <luciano.giuliani92@gmail.com>
2022-06-29 18:37:24 -0300

    Comienzo EJS

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const {resolve} = require('path');
+
 
 const {port, callback} = require("../modules/listen.js");
 
 const public = require('../modules/public.js');
 app.use(public)
 
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+
 app.listen(port, callback(`Abriendo servidor en http://localhost:` + port))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/index.html')));
-app.get('/productos', (req, res) => res.sendFile(path.join(__dirname, '/views/productDetail.html')));
-app.get('/buscador', (req, res) => res.sendFile(path.join(__dirname, '/views/productSearch.html')));
-app.get('/carrito', (req, res) => res.sendFile(path.join(__dirname, '/views/productCart.html')));
-app.get('/register', (req, res) => res.sendFile(path.join(__dirname, '/views/register.html')));
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, '/views/login.html')));
+app.get('/', (req, res) => res.render(path.join(__dirname, './views/index.ejs')));
+app.get('/productos', (req, res) => res.render(path.join(__dirname, '/views/productDetail.ejs')));
+app.get('/buscador', (req, res) => res.render(path.join(__dirname, '/views/productSearch.ejs')));
+app.get('/carrito', (req, res) => res.render(path.join(__dirname, '/views/productCart.ejs')));
+app.get('/register', (req, res) => res.render(path.join(__dirname, '/views/register.ejs')));
+app.get('/login', (req, res) => res.render(path.join(__dirname, '/views/login.ejs')));
 
