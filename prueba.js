b9c4410a2fa1204ead115cc50c6e1f99f6cd2474
“Esteban-Somoza” <estfersomoza@gmail.com>
2022-07-10 22:44:58 -0300

    pagina de productos dinamica

---
+++ prueba.js
@@ -0,0 +1,37 @@
+let name = "name"
+let apellido = "apellido"
+let edad = "edad"
+
+let prod = {
+    [name]: "juan",
+    [apellido]: "lopez",
+    [edad]: 4
+}
+
+console.log(prod)
+
+
+let informacion = ["Apto",
+    "Color",
+    'Diseño',
+    'Línea',
+    'Presentación',
+    'Tecnología',
+    'Tipo',
+    'Estilos']
+
+let info = ['Apto termotanque y calefón',
+    'Cromo, Rose gold',
+    'Volante Cruz',
+    '',
+    'Cierre suave',
+    'Cierre cerámico',
+    'Tradicional',
+    'Industrial, minimalista, moderno',]
+
+for (let i = 0; i < info.length; i++) {
+    if (info[i].length != 0) {
+        console.log(informacion[i] + ":" + info[i]);
+    }
+}
+
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,36 +1,42 @@
-const { index } = require("../models/product.model");
+const { index, find } = require("../models/product.model");
 module.exports = {
 
   productDetail: (req, res) => {
+    let product = find(parseInt(req.params.id))
+    if (!product) {
+      return res.redirect('/products')
+    }
+    let info = product.informacion
     return res.render("./products/productDetail", {
       title: "Product Detail",
-      products: index,
+      product: product,
     });
   },
   productSearch: (req, res) => {
     return res.render("./products/productSearch", {
       title: "Product Search",
-      products: index,
+      products: index(),
     });
   },
   productCart: (req, res) => {
     return res.render("./products/productCart", {
-        title: "Product Cart",
-        products: index,
+      title: "Product Cart",
+      products: index(),
     })
   },
   productCreate: (req, res) => {
     return res.render("./products/create", {
-        title: "Product Create",
-        products: index,
+      title: "Product Create",
+      products: index(),
     })
   },
   productSave: (req, res) => {
     let id = req.params.id;
     return res.render(`./products/${id}`, {
-        title: "Product Create",
-        products: index,
+      title: "Product Create",
+      products: index(),
     })
   },
- 
+
+
 };
