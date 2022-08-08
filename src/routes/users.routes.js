7b336a7b95ff21a2e7f1c08877958a34b77ce95c
LuchoGiuliani <luciano.giuliani92@gmail.com>
2022-08-08 17:12:55 -0300

    Update

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,14 +2,14 @@ const { Router } = require("express")
 const router = Router()
 const middlewareRegister = require("../middlewares/register");
 const middlewareLogin = require("../middlewares/login.js");
-const isLogged = require('../middlewares/isLogged')
+
 
 const { login, register, process, access, logout } = require('../controllers/users.controller');
 
 // login
 router.get("/users/login", login)
 router.post('/users/access', middlewareLogin, access)
-router.post('/users/logout',[isLogged],logout)
+router.post('/users/logout',logout)
 
 // register
 router.get("/users/register", register)
