d8ad4dd582ec53914da3f814a504b81fbd1ef6f1
Esteban-Somoza <estfersomoza@gmail.com>
2022-08-22 20:38:48 -0300

    primer seed creado

--- src/database/seeders/20220822225157-Image.js
+++ src/database/seeders/20220822225157-Image.js
@@ -1,24 +1,20 @@
 'use strict';
 
+const { index } = require('../../models/users.model')
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    let image = index().map(user => {
+      let userimage = {
+        nombre: user.imagenId,
+      }
+      return userimage
+    })
+    await queryInterface.bulkInsert('imagen', image, {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('usuarios', null, {});
   }
 };
+
