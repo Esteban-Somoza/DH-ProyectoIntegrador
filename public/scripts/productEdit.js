31a4b827eb99b16ba6919a3f279774ea48eb1e10
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2022-09-12 19:10:35 -0300

    productCreate y productEdit

---
+++ public/scripts/productEdit.js
@@ -0,0 +1,19 @@
+
+// let document.querySelectorById('formulario');
+ let forms = Document.forms.formulario
+ let inputs = forms.elements
+ console.log(inputs)
+ inputs.nombre.addEventListener('input', function (e) {{
+    let value= e.target.value;
+      let nombre = e.target.parentElement // color 
+      let feed = formulario.querySelector('.formulario')
+      let msg= null;
+      if (!validator.isLength(value,{min:5}))
+      msg= 'No hay suficientes caracteres';
+       if (msg) {
+ 
+
+      
+       }
+      }
+    })
