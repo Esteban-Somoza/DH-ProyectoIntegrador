0761e36a55ca0adc238d14261befb76f3e8a111f
Esteban-Somoza <estfersomoza@gmail.com>
2022-10-01 14:34:02 -0300

    statistics - info en vez de data.data

--- dashboard/src/includes/Statistics.jsx
+++ dashboard/src/includes/Statistics.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 export default function Statistics({title, data}) {
-  let dataArray = data.data
+  let info = data.data
   console.log(data.data);
   return (
     <div>
       <h3>Total {title} Count</h3>
       {
-        data.data.nombre
+        info.nombre
       }
     </div>
   )
