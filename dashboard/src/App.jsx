40c99c2e150e1094680f172f306f3f82143a5718
Esteban-Somoza <estfersomoza@gmail.com>
2022-10-01 11:43:19 -0300

    product overview terminado y comienzo home

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -22,11 +22,11 @@ function App() {
       {/* <Link to="/">Home</Link> */}
       <Routes>
         {/* <Route exact path="/" element={<PublicLogin />}/> */}
-        <Route exact path="/" element={!loggedIn ? <PublicLogin /> : <Home />}/>
+        {/* <Route exact path="/" element={!loggedIn ? <PublicLogin /> : <Home />}/> */}
+        <Route exact path="/" element={<Home />}/>
         <Route exact path="/products/:category" element={<ProductsOverview />}/>
         {/* <Route path="/" element={<PublicLogin />}></Route> */}
         <Route path="/usuarios" element={ <User/> } />
-        {/* <Route exact path="/" element={<Home />}/> */}
         <Route path="*" element={<Error />}></Route>
         <Route exact path="/products/detail/:id" element={<ProductDetail />}/>
       </Routes>
